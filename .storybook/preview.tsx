dcc5b1de22ca15b7ae14519bf8f58be9d4508af3
mrphemi <mrlapinho@gmail.com>
2025-08-01 02:09:10 +0100

    Add storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,32 @@
+import { League_Spartan } from "next/font/google";
+
+import type { Preview } from "@storybook/nextjs";
+
+import "../src/app/globals.css";
+
+const spartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--font-league-spartan",
+});
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <div className={`${spartan.className} antialiased`}>
+          <Story />
+        </div>
+      );
+    },
+  ],
+};
+
+export default preview;
---
+++ src/ui/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button className="bg-red-400">Button</button>;
+};
+
+export default Button;
